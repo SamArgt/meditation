c1a3551ed81881ebe3db23754bcc9ecde1b4bdd1
SamArgt <36817478+SamArgt@users.noreply.github.com>
2026-02-22 20:09:24 +0100

    network offline

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -1,6 +1,6 @@
 import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
-import { Serwist } from "serwist";
+import { ExpirationPlugin, NetworkFirst, Serwist } from "serwist";
 
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
@@ -10,6 +10,20 @@ declare global {
 
 declare const self: ServiceWorkerGlobalScope;
 
+const fastOfflineNavigation = {
+  matcher: ({ request }: { request: Request }) => request.mode === "navigate",
+  handler: new NetworkFirst({
+    cacheName: "pages",
+    networkTimeoutSeconds: 3,
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 32,
+        maxAgeSeconds: 24 * 60 * 60,
+      }),
+    ],
+  }),
+};
+
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   precacheOptions: {
@@ -17,8 +31,8 @@ const serwist = new Serwist({
   },
   skipWaiting: true,
   clientsClaim: true,
-  navigationPreload: true,
-  runtimeCaching: defaultCache,
+  navigationPreload: false,
+  runtimeCaching: [fastOfflineNavigation, ...defaultCache],
 });
 
 serwist.addEventListeners();
